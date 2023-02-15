4ccd2b15f1805bf495ef543ca550593f6728e559
Joaquin Alonso <Joaquin.Alonso@partner.bmwgroup.com>
2023-02-15 13:30:53 +0100

    startify: change vcsto root

--- lua/plugins/configs/startify.lua
+++ lua/plugins/configs/startify.lua
@@ -1,2 +1,3 @@
 -- vim.cmd[[ let g:startify_custom_header = 'startify#center(startify#fortune#cowsay())' ]]
 vim.cmd[[ let g:startify_padding_left = 40 ]]
+vim.cmd[[ let g:startify_change_to_vcs_root = 1 ]]
